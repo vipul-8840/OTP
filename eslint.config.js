b54b6027b1c2785757ea07b932ad3fe95379dcca
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-04 10:42:38 +0530

    input field controlled

--- eslint.config.js
+++ eslint.config.js
@@ -28,6 +28,7 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'rule/prop-types':"off"
     },
   },
 ]
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+const digitCount = 5;
 const App = () => {
+  const [inputArr,setInputArr] = useState(new Array(digitCount).fill(""));
+  function handleOnChnage(value,index){
+         if(isNaN(value))
+         {
+            return;
+         }
+        const newArray =[...inputArr];
+        newArray[index]=value.slice(-1)
+        setInputArr(newArray)
+  }
   return (
     <div>
        <h1 className='flex justify-content justify-center items-center text-lg font-medium'>Validate Input</h1>
+        <div className='flex gap-2 justify-center mt-4 text-lg'>
+        {inputArr.map((input, index) => (
+          <input
+            key={index}
+            type='text'
+            value={input}
+            className='border rounded-sm border-gray-300 w-12 h-12 text-center'
+            onChange={(e)=>handleOnChnage(e.target.value,index)}
+           
+          />
+        ))}
+        </div>
     </div>
   )
 }
