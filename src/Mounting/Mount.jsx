4e19c3a9645cccf3c29f0d83d7c5c5050ef62352
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-04 19:04:56 +0530

    react lifecycle Methods

---
+++ src/Mounting/Mount.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Mount extends Component {
+  render() {
+    return (
+      <div className='font-medium flex justify-center  text-3xl items-center min-h-screen'>
+          hello vipul!
+      </div>
+    )
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import Ref from './Ref.jsx'
 import Toggle from './toggle.jsx'
+import Mount from './Mounting/Mount.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <Ref/> */}
-    <Toggle/>
+    <Mount/>
   </StrictMode>,
 )
