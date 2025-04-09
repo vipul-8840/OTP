aad1f883798d0513f0e6a92081f724559e50935b
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-09 19:13:28 +0530

    header,layout

---
+++ src/Router/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../Header/header'
+import { Outlet } from 'react-router-dom'
+import Footer from '../Footer/footer'
+
+const Layout = () => {
+  return (
+    <div>
+        <Header/>
+        <Outlet/>
+        <Footer/>
+      
+    </div>
+  )
+}
+
+export default Layout
