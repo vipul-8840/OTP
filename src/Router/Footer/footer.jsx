e382163e72a9bee3dcbd0311aa302ba62f58b3b6
vipul-8840 <vipulkumarlen@gmail.com>
2025-04-09 19:10:41 +0530

    footer component

--- src/Router/Footer/footer.jsx
+++ src/Router/Footer/footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const footer = () => {
+const Footer = () => {
   return (
     <div>
-      
+         Footer
     </div>
   )
 }
 
-export default footer
+export default Footer;
